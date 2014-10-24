d4b0e81e6f8483805ad7a39b733d4e29e06cf75c
Christopher <christopher.martinez@vohfix.com>
2014-10-24 01:01:34 -0500

    Update snap.php
    Updated comments to make sure they were understandable

--- snap.php
+++ snap.php
@@ -2,31 +2,30 @@
 <body>
 <?php 
 
+//Location of the snapchat API files
 include 'snap/phpsnap/src/snapchat.php';
 
 // Log in:
-$snapchat = new Snapchat('yotobooth', 'todo1117');
+$snapchat = new Snapchat('SNAPCHATUSER', 'PASSWORD');
 
-// Upload a snap and send it to me for 8 seconds:
+//Send Snap to story
 $id = $snapchat->upload(
     Snapchat::MEDIA_IMAGE,
     file_get_contents('img/img001.jpg')
 );
 $snapchat->setStory($id, Snapchat::MEDIA_IMAGE);
 
-// Upload a snap and send it to me for 8 seconds:
+//used for testing purposes to see if the snapchats were
+//being sent but can be removed once done testing
 $id = $snapchat->upload(
     Snapchat::MEDIA_IMAGE,
     file_get_contents('img/img001.jpg')
 );
-$snapchat->send($id, array('mrmuffins17'), 8);
+$snapchat->send($id, array('SNAPCHATUSER'), 8);
 
-
-
-
-echo "done";
+echo " ";
 ?>
 
-uploaded!
+<h1>uploaded!</h1>
 <img src="img/img001.jpg" />
 </body>
