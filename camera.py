cc120cb8c76693784a1bb493bfdb1929f413840f
Christopher <christopher.martinez@vohfix.com>
2014-10-24 01:03:33 -0500

    Camera Python Script
    Removed some unnecessary comments

--- camera.py
+++ camera.py
@@ -3,7 +3,6 @@ import time, picamera
 with picamera.PiCamera() as camera:
     # Initial settings of the camera
     camera.preview_fullscreen = True
-   # camera.resolution = (100, 100)
     camera.preview_window = (620, 320, 640, 480)
     camera.exposure_mode = 'auto'
     camera.vflip = True
@@ -12,10 +11,4 @@ with picamera.PiCamera() as camera:
     camera.start_preview()
     time.sleep(3) # Waiting for the camera to adjust
     camera.capture('img/img001.jpg')
-   # camera.shutter_speed = 6000000
-   # camera.exposure_mode = 'off'
-   # camera.iso = 800
-   #for filename in camera.capture_continuous('img/img{counter:03d}.jpg'):
-        #print('Captured %s' % filename)
-        #time.sleep(3) # wait 5 seconds between each shot
 
